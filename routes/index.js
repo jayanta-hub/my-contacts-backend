c2817b2f5dc4f4d2e804910cd695e6847faabc65
jayanta <jayanta.garu@arithaconsulting.com>
2023-11-08 14:31:44 +0530

    wdcw

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,5 @@ const express = require("express");
 const mainRoute = express.Router();
 mainRoute.use("/users/contacts", require("./contactsRoutes"));
 mainRoute.use("/authenticate", require("./userRoutes"));
+mainRoute.use("/user", require("./ProfileRoutes"));
 module.exports = mainRoute;
